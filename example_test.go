b456e745550193b55cc494442700b1fe7d5342cc
tsatke <tim.satke@gmail.com>
2020-12-02 23:53:48 +0100

    Improve example

--- example_test.go
+++ example_test.go
@@ -7,9 +7,7 @@ import (
 )
 
 func ExampleEngine_EvalString() {
-	source := `
-print("Hello, World!")
-`
+	source := `print("Hello, World!")`
 	e := NewEngine(
 		WithStdout(os.Stdout),
 	)
@@ -23,9 +21,7 @@ print("Hello, World!")
 func ExampleEngine_Eval() {
 	// this can be every file from os.Open or (afero.Fs).Open, which all implement the
 	// io.Reader interface
-	var sourceFile io.Reader = strings.NewReader(`
-print("Hello, World!")
-`)
+	var sourceFile io.Reader = strings.NewReader(`print("Hello, World!")`)
 	e := NewEngine(
 		WithStdout(os.Stdout),
 	)
